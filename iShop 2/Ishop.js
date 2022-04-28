0579168d421a3ad9a418746129ff34ce4b3ced94
gribchic <gribchic@gmail.com>
2022-04-28 20:41:45 +0300

    feat: homework 3. Ishop2 select/delete toy

---
+++ iShop 2/Ishop.js
@@ -0,0 +1,69 @@
+﻿var ToysBlock = React.createClass({
+
+  displayName: 'ToysBlock',
+
+  propTypes: {
+    shopName: React.PropTypes.string.isRequired,
+    toys: React.PropTypes.arrayOf(
+      React.PropTypes.shape({
+        id: React.PropTypes.number.isRequired,
+        price: React.PropTypes.number.isRequired,
+        name: React.PropTypes.string.isRequired,
+        amount: React.PropTypes.number.isRequired,
+        imgUrl: React.PropTypes.string.isRequired,
+      })
+    )
+  },
+
+  getInitialState: function () {
+    return {
+      toys: [...this.props.toys],
+      selectedToyId: null,
+    };
+  },
+
+  onSelectToyHandler: function (id) {
+    this.setState({ selectedToyId: id })
+  },
+
+  onDeleteToyHandler: function (id) {
+    if (confirm('are you sure you want to delete this toy?')) {
+      console.log(id);
+      this.setState((prevState) => {
+        console.log(prevState.toys.filter(toy => toy.id !== id));
+        return {toys: prevState.toys.filter(toy => toy.id !== id)}
+      } )
+    }
+  },
+
+  render: function () {
+    //console.log(this.state.toys);
+
+    if(this.state.toys.length===0) {
+      return React.DOM.div({ className: 'IshopBlock' },
+      React.DOM.h1({ className: 'h1' }, this.props.shopName),
+      React.DOM.div({ className: 'alert' }, 'Sorry. There are no toys!'),
+    );
+    }
+
+    var toysCode = this.state.toys.map(v =>
+
+      React.DOM.div({ className: 'toy-container', key: v.id },
+        React.createElement(Toy, {
+          name: v.name,
+          price: v.price,
+          amount: v.amount,
+          imgUrl: v.imgUrl,
+          isSelected: this.state.selectedToyId === v.id,
+          onSelectToy: this.onSelectToyHandler,
+          onDeleteToy: this.onDeleteToyHandler,
+          id: v.id,
+        }),
+      ),
+    );
+    return React.DOM.div({ className: 'IshopBlock' },
+      React.DOM.h1({ className: 'h1' }, this.props.shopName),
+      React.DOM.div({ className: 'row' }, toysCode),
+    );
+  },
+});
---
+++ iShop 2/Toy.js
@@ -0,0 +1,48 @@
+let Toy = React.createClass({
+
+  displayName: 'Toy',
+
+  propTypes: {
+    name: React.PropTypes.string.isRequired,
+    price: React.PropTypes.number.isRequired,
+    amount: React.PropTypes.number.isRequired,
+    imgUrl: React.PropTypes.string.isRequired,
+    isSelected: React.PropTypes.bool.isRequired,
+    onSelectToy: React.PropTypes.func.isRequired,
+    onDeleteToy: React.PropTypes.func.isRequired,
+    id: React.PropTypes.number.isRequired,
+  },
+
+  onSelectToyHandler: function (event) {
+    if (event.target.tagName === 'BUTTON') {
+      return;
+    };
+
+    console.log('select');
+    this.props.onSelectToy(this.props.id);
+  },
+
+  onDeleteToyHandler: function (event) {
+    console.log('delete');
+    this.props.onDeleteToy(this.props.id);
+  },
+
+  className: function () {
+    return `Toy ${this.props.isSelected ? 'selected' : ''}`;
+  },
+
+  render: function () {
+    return React.DOM.section({ className: this.className(), onClick: this.onSelectToyHandler },
+      React.DOM.h2(null, this.props.name),
+      React.DOM.img({ src: this.props.imgUrl, className: 'img' }),
+      React.DOM.div({ className: 'description' },
+        React.DOM.span({ className: 'price' }, `Price: $${this.props.price}`),
+        React.DOM.span({ className: 'amount' }, `Amount: ${this.props.amount}`),
+      ),
+      React.DOM.button({
+        type: 'button',
+        onClick: this.onDeleteToyHandler
+      }, 'Delete'),
+    )
+  }
+});
